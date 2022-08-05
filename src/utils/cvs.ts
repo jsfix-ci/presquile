56a6503ca21601b20ccd3fd8b8d7cde08fa56db5
jsfix.live analysis <info@coana.tech>
2022-08-05 14:13:44 +0200

    JSFIX patches

--- src/utils/cvs.ts
+++ src/utils/cvs.ts
@@ -1,4 +1,4 @@
-import parses from 'csv-parse/lib/sync'
+import parses from 'csv-parse/sync'
 import { Either, left, right } from 'fp-ts/lib/Either'
 import { Options } from 'csv-parse'
 
@@ -8,7 +8,7 @@ const PARSE_ERROR_MESSAGE = 'This is not CVS content'
 /* eslint-disable  @typescript-eslint/explicit-module-boundary-types */
 export function parse(data:Buffer|string, options:Options):Either<Error, any>{
     try {
-        const parsedData = parses(data, options)
+        const parsedData = parses.parse(data, options)
         return (parsedData !== undefined && parsedData.length > 0) ? right(parsedData) : left(Error(PARSE_ERROR_MESSAGE))
     } catch (e){
         return left(Error(PARSE_ERROR_MESSAGE))
